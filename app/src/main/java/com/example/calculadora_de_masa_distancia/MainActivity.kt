3382d9176f44a9b3da4728fdc2f221c7930fe214
Adelfo <adelfodangf@gmail.com>
2025-08-21 17:31:19 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calculadora_de_masa_distancia/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.calculadora_de_masa_distancia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculadora_de_masa_distancia.ui.theme.Calculadora_de_masa_distanciaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Calculadora_de_masa_distanciaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Calculadora_de_masa_distanciaTheme {
+        Greeting("Android")
+    }
+}
